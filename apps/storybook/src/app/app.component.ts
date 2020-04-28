0013ca5bfd4e2f3ac69d1ad75e385b3f75249c47
vikramnagineni <vikramnagineni@outlook.com>
2020-04-28 16:51:29 +0530

    next commit

---
+++ apps/storybook/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kiran-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'storybook';
+}
